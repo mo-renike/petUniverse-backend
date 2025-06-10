0740cf677164b83c9461f6f5532e0d9d99deec76
Arthakker <arthuronyeanusi@gmail.com>
2025-06-10 16:53:01 +0100

    Completed routes for post and comments, included testing with hardcoded user in MySQL and included console messages for better error logging

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -5,18 +5,27 @@ const prisma = new PrismaClient
 export const createComment = async(req, res)=>{
     const {content, authorId}  = req.body
 
-    const {postId} = req.params
+    const postId = req.params.postid;
 
     try {
+        
+
         const comments = await prisma.comment.create({
-            data:{
-                content,
-                postId,
-                authorId,
-            }
-        })
+  data: {
+    content,
+    post: {
+      connect: { id: parseInt(postId) }
+    },
+    author: {
+      connect: { id: authorId }
+    }
+  }
+});
+
         res.status(201).json(comments)
     } catch (error) {
+        console.log("Failed to create coment because: ", error);
+        
         res.status(501).send("Could not create new comment ")
     }
 }
