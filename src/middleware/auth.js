179f6adc48423d7fa9f5bab88a20fb40a755c54e
Morenike <herroyalpianist@gmail.com>
2025-06-24 19:48:00 +0100

    feat: add posts route and fix authorization header case sensitivity
    - Integrated `postRoutes` into the main application in `index.js` to handle posts API.
    - Updated the authorization header key in `auth.js` to be case-insensitive by changing "Authorization" to "authorization".

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 import { prisma } from "../utils/prisma.js";
 
 export const authenticateToken = async (req, res, next) => {
-  const authHeader = req.headers["Authorization"];
+  const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
   if (!token) {
